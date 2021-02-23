0e0e094c4f7e1f55bd99778dd886f9a419a995bb
othmanamr <amr.othman@patelhs.org>
2021-02-23 14:13:12 +0000

    added count

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
 
+import javax.sound.midi.ShortMessage;
+
 class Main {
   public static void main(String[] args) {
+    count = 0;
     Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome to charity area");
-    System.out.println("how much money would you like to donate?");
+    System.out.println("Welcome to shoe stone");
+    System.out.println("how much shoes would you like to buy by the way every shoe is 20$?");
     int dollar = scan.nextInt();
     saydollar(dollar);
   }
 
   public static void saydollar(int q) {
     if (q == 0) {
-      
-
+   System.out.println("how much money do you have?");    
+   double money = scan.nextDouble();
+   if(money < 20.0){
+    System.out.println("sorry but you cannot afford any of these shoes");
+   } else {
+    System.out.println("you can now afford one shoe, would you like to purchase");
+    shoes(100);
+    if ( <20)
+    count = count + 1;
+   }
+  
+   
     } else {
       System.out.println("dollar");
       q--;
